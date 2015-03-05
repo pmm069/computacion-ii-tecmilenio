807d975e159ffea82aff44764bf3b0f2a424d303
Juan.Gonzalez84@gmail.com <Juan.Gonzalez84@gmail.com@d0301e70-707b-a038-3ad6-373c017417c4>
2015-03-05 21:19:43 +0000

    Agrego archivos para ejercicio en clase Tema 6

--- trunk/manejoExcepciones/src/src/ejemploFinally.java
+++ trunk/manejoExcepciones/src/src/ejemploFinally.java
@@ -1,3 +1,5 @@
+package src;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -33,4 +35,4 @@ public class ejemploFinally {
     
     
     
-}
+
