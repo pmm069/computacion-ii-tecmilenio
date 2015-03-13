c62c301458b016f79653da85a7d46581e15d1279
Juan.Gonzalez84@gmail.com <Juan.Gonzalez84@gmail.com@d0301e70-707b-a038-3ad6-373c017417c4>
2015-03-13 02:13:43 +0000

    v

--- trunk/collections/ComparatorBMain.java
+++ trunk/collections/ComparatorBMain.java
@@ -1,3 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase8;
+
+/**
+ *
+ * @author User
+ */
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -5,48 +15,48 @@ import java.util.List;
 
 public class ComparatorBMain {
 
-	/**
-	 * @author Arpit Mandliya
-	 */
-	public static void main(String[] args) {
-		 CountryB indiaCountry=new CountryB(1, 'India');
-		 CountryB chinaCountry=new CountryB(4, 'China');
-		 CountryB nepalCountry=new CountryB(3, 'Nepal');
-		 CountryB bhutanCountry=new CountryB(2, 'Bhutan');
+        /**
+         * @author Arpit Mandliya
+         */
+        public static void main(String[] args) {
+                 CountryB indiaCountry=new CountryB(1, "India");
+                 CountryB chinaCountry=new CountryB(4, "China");
+                 CountryB nepalCountry=new CountryB(3, "Nepal");
+                 CountryB bhutanCountry=new CountryB(2, "Bhutan");
 
-	        List<CountryB> listOfCountries = new ArrayList<CountryB>();
-	        listOfCountries.add(indiaCountry);
-	        listOfCountries.add(chinaCountry);
-	        listOfCountries.add(nepalCountry);
-	        listOfCountries.add(bhutanCountry);
+                List<CountryB> listOfCountries = new ArrayList<CountryB>();
+                listOfCountries.add(indiaCountry);
+                listOfCountries.add(chinaCountry);
+                listOfCountries.add(nepalCountry);
+                listOfCountries.add(bhutanCountry);
 
-	        System.out.println('Before Sort by id : ');
-	        for (int i = 0; i < listOfCountries.size(); i++) {
-				CountryB country=(CountryB) listOfCountries.get(i);
-				System.out.println('Country Id: '+countryB.getCountryId()+'||'+'Country name: '+countryB.getCountryName());
-			}
-	        Collections.sort(listOfCountries,new CountrySortByIdComparator());
+                System.out.println("Before Sort by id : ");
+                for (int i = 0; i < listOfCountries.size(); i++) {
+                                CountryB countryB=(CountryB) listOfCountries.get(i);
+                                System.out.println("Country Id: "+countryB.getCountryBId()+"||"+"Country name: "+countryB.getCountryName());
+                        }
+                Collections.sort(listOfCountries,new CountryBSortByIdComparator());
 
-	        System.out.println('After Sort by id: ');
-	        for (int i = 0; i < listOfCountries.size(); i++) {
-				CountryB country=(CountryB) listOfCountries.get(i);
-				System.out.println('Country Id: '+countryB.getCountryId()+'|| '+'Country name: '+countryB.getCountryName());
-			}
+                System.out.println("After Sort by id: ");
+                for (int i = 0; i < listOfCountries.size(); i++) {
+                                CountryB countryB=(CountryB) listOfCountries.get(i);
+                                System.out.println("Country Id: "+countryB.getCountryBId()+"|| "+"Country name: "+countryB.getCountryName());
+                        }
 
-	        //Sort by countryName
-	        Collections.sort(listOfCountries,new Comparator<CountryB>() {
+                //Sort by countryName
+                Collections.sort(listOfCountries,new Comparator<CountryB>() {
 
-				@Override
-				public int compare(CountryB o1, CountryB o2) {
-					return o1.getCountryName().compareTo(o2.getCountryName());
-				}
-			});
+                                @Override
+                                public int compare(CountryB o1, CountryB o2) {
+                                        return o1.getCountryName().compareTo(o2.getCountryName());
+                                }
+                        });
 
-			System.out.println('After Sort by name: ');
-	        for (int i = 0; i < listOfCountries.size(); i++) {
-				CountryB country=(CountryB) listOfCountries.get(i);
-				System.out.println('Country Id: '+countryB.getCountryId()+'|| '+'Country name: '+countryB.getCountryName());
-			}
-	}
+                        System.out.println("After Sort by name: ");
+                for (int i = 0; i < listOfCountries.size(); i++) {
+                                CountryB countryB=(CountryB) listOfCountries.get(i);
+                                System.out.println("Country Id: "+countryB.getCountryBId()+"|| "+"Country name: "+countryB.getCountryName());
+                        }
+        }
 
 }
