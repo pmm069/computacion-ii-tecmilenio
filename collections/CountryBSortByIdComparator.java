c51769d66cce69bcb31059b9b9e62b496b413425
Juan.Gonzalez84@gmail.com <Juan.Gonzalez84@gmail.com@d0301e70-707b-a038-3ad6-373c017417c4>
2015-03-13 02:09:02 +0000

    we

--- collections/CountryBSortByIdComparator.java
+++ collections/CountryBSortByIdComparator.java
@@ -1,14 +1,10 @@
-
-//If country1.getCountryId()<country2.getCountryId():then compare method will return -1
-//If country1.getCountryId()>country2.getCountryId():then compare method will return 1
-//If country1.getCountryId()==country2.getCountryId():then compare method will return 0
  
  public class CountryBSortByIdComparator implements Comparator<CountryB>{
 
     @Override
     public int compare(CountryB country1, CountryB country2) {
 
-        return (country1.getCountryId() < country2.getCountryId() ) ? -1: (country1.getCountryId() > country2.getCountryId() ) ? 1:0 ;
+        return (country1.getCountryBId() < country2.getCountryBId() ) ? -1: (country1.getCountryBId() > country2.getCountryBId() ) ? 1:0 ;
     }
 
 }
